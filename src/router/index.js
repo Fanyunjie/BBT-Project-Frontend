40c337e0510f16f8ad1002be55896fa2b50cedf9
Ryze0 <1123792770@qq.com>
2020-11-16 11:27:23 +0800

    加入了注册界面的UI界面，不过注册逻辑还没写好， 输入8080/signin可跳转 下一步我接着在login界面写个注册按钮跳转注册界面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/components/Login'
+import Signin from "@/components/Signin";
 
 Vue.use(VueRouter)
 
  const routes = [
    { path: '/', redirect: 'login'},
     {path:'/login',component:Login},
+    {path: '/signin',component: Signin}
+
  ]
 
  const router = new VueRouter({
+   mode:'history',
   routes
 })
 
